694bef3d2dd8562dc154e3631e0d076f5ec4ffe4
healyt22 <healyt22@gmail.com>
2017-08-18 22:24:11 -0400

    updated R file

--- scrape.R
+++ scrape.R
@@ -2,6 +2,8 @@ library(rvest)
 library(plyr)
 library(stringr)
 
+setwd("~/G_WD/SIF_scrape")
+
 system("./phantomjs scrape_oddshark.js")
 
 html = read_html('oddsshark.html')
@@ -22,3 +24,6 @@ lines = str_split_fixed(lines, " ", 2)
 
 spreads = as.data.frame(cbind(teams, lines))
 colnames(spreads) = c("away", "home", "away_spread", "home_spread")
+spreads$timestamp = Sys.time()
+
+write.csv(spreads, paste("./output/Spreads_", Sys.Date(),".csv", sep = ""), row.names = FALSE)
