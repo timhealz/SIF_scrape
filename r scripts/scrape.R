cb9b408664784d5004f962ae2dd31aa6571d7a90
healyt22 <healyt22@gmail.com>
2017-08-29 23:39:25 -0700

    8.29.2017 file added

--- r scripts/scrape.R
+++ r scripts/scrape.R
@@ -19,7 +19,7 @@ oddshark = read.csv(
                                                                         "oddshark_spread")]
 
 dat = merge(espn, oddshark, by = "game_id", all.x = TRUE)
-dat = dat[order(dat$date, dat$time),]
+dat = dat[order(dat$game_id),]
 dat$update_ts = Sys.time()
 
 write.csv(dat, paste(out_path, "/", "Spreads_", Sys.Date(), ".csv", sep = ""), row.names = FALSE)
